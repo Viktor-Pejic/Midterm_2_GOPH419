fc5f1a8339634d03870e174e98da8eac86e6c2be
Viktor Pejic <viktor.pejic@ucalgary.ca>
2024-12-04 19:40:03 -0700

    added cubic spline interpolation of CO2 concentration

--- src/Question_1_code.py
+++ src/Question_1_code.py
@@ -1,14 +1,18 @@
 
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 
-
-
-def cubic_spline(x):
+def cubic_spline(x, xd, yd):
     x = np.array(x, dtype=float)
     if np.any(x < xd[0]) or np.any(x > xd[-1]):
         raise ValueError(f"Input value(s) out of bounds: [{xd[0]}, {xd[-1]}].")
     n = len(xd) - 1
+
+    delta_x = np.diff(xd)
+    delta_y = np.diff(yd)
+    div_dif_1 = delta_y / delta_x
     div_dif_2 = np.diff(div_dif_1)
 
     rhs = np.zeros(n + 1)
@@ -37,3 +41,22 @@ def cubic_spline(x):
 
     y = a[i] + b[i] * dx + c[i] * dx ** 2 + d[i] * dx ** 3
     return y
+
+def main():
+    CO2 = np.loadtxt("C:/Users/Viktor/repos/Midterm_2_GOPH419/data/CO2_data.txt", 'float')
+    xd = np.array(CO2[51:62,0], dtype='float') #Year on the x-axis
+    yd = np.array(CO2[51:62,1], dtype='float') #CO2 concentration on the y-axis
+
+    x = np.linspace(2010,2020,10)
+
+    spline = cubic_spline(x, xd, yd)
+
+    plt.plot(x, spline)
+    plt.scatter(xd, yd, color='black')
+    plt.xlabel('Years')
+    plt.ylabel('CO2 Concentration (mean)')
+    plt.title('CO2 Concentration Trend from 2010-2020', fontweight='bold')
+    plt.show()
+
+if __name__ == '__main__':
+    main()
